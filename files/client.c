684197596b6ae6a5a77ece984bf190e6b2828b49
CRESPY Arthur <arthur@crespy.eu>
2023-02-01 14:36:50 +0100

    Use of sigaction() instead of signal()

--- files/client.c
+++ files/client.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 08:29:48 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/01 09:25:40 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/01 14:35:35 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,11 @@ void	send_bits(int pid, char *str)
 		bit = 0;
 		while (bit < 8)
 		{
+			usleep(100);
 			if ((c & (0x01 << bit)) != 0)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(100);
 			bit++;
 		}
 		i++;
@@ -62,14 +62,18 @@ void	receive_confirmation(int sig, siginfo_t *siginfo, void *content)
 
 int	main(int argc, char **argv)
 {
-	int	pid;
+	struct sigaction	sig;
 
+	sig.sa_sigaction = receive_confirmation;
+	sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sig, NULL);
 	if (argc < 3 || argc > 3)
 	{
-		write(1, "Error: wrong number of arguments", 32);
+		write(1, "> Error: wrong number of arguments", 33);
 		return (1);
 	}
-	pid = ft_atoi(argv[1]);
-	send_bits(pid, argv[2]);
+	send_bits(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		;
 	return (0);
 }
