01258d5145be9cfc005ae7b9906defdd9c3d5239
CRESPY Arthur <arthur@crespy.eu>
2023-02-02 16:10:36 +0100

    Move linked lists functions in lists.c

---
+++ files/lists.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lists.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 08:28:53 by acrespy           #+#    #+#             */
+/*   Updated: 2023/02/02 15:52:20 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	clear_list(t_struct *message)
+{
+	t_struct	*tmp;
+
+	while (message != NULL)
+	{
+		tmp = message;
+		message = message->next;
+		free(tmp);
+	}
+}
+
+void	print_list(t_struct *message)
+{
+	t_struct	*tmp;
+
+	tmp = message;
+	while (tmp != NULL)
+	{
+		ft_putchar_fd(tmp->data, 1);
+		tmp = tmp->next;
+	}
+	ft_putchar_fd('\n', 1);
+}
